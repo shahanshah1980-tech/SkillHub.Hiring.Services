04810c775d87ad7a409ed4f2a215f263be97aa30
shahanshah1980-tech <shahanshah1980@gmail.com>
2026-03-09 23:46:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-CREATE DATABASE skillhub;
 
-USE skillhub;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    fullname VARCHAR(100) NOT NULL,
-    mobile VARCHAR(20) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE otp_log (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    otp VARCHAR(6) NOT NULL,
-    created_at DATETIME NOT NULL
-);
